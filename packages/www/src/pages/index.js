2d0c83e6ff89213f73dbfc45ca3bb778257fe1b9
Sacha <sacha@annex.io>
2020-07-16 15:25:11 -0500

    Theme Netlify Auth

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,6 +1,29 @@
-import React from "react";
-export default props => (
-    <div>
-    <h1>Our Site</h1>
-</div>
-);
+import React, {useEffect} from "react";
+import { Container, Heading, Button, Flex, NavLink } from "theme-ui";
+import netlifyIdentity from "netlify-identity-widget";
+
+export default props => {
+    useEffect(() => {
+        netlifyIdentity.init({});
+    });
+    return (
+        <Container>
+            <Flex sx ={{flexDirection: "column", padding: 3}}>
+                <Heading>Our Site</Heading>
+                <Button sx={{ marginTop: 2 }}
+                        onClick={() => {
+                            netlifyIdentity.open();
+                        }}>
+                    Log In
+                </Button>
+                <Button sx={{ marginTop: 2 }}
+                        onClick={() => {
+                            console.log(netlifyIdentity.currentUser());
+                        }}>
+                    Log User
+                </Button>
+            </Flex>
+        </Container>
+    );
+}
+
